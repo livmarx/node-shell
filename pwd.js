482dce7c4a04f5bfe9d7d9e48a1066fa91923cd5
Liv Marx <oehrenmarks@gmail.com>
2018-09-18 12:52:10 -0400

    added prompt and pwd comsnd

--- pwd.js
+++ pwd.js
@@ -1,10 +1,8 @@
-process.stdout.write('prompt > ');
-
-process.stdin.on('data', data => {
+const pwd = process.stdin.on('data', data => {
   if (data.toString().trim() === 'pwd') {
     process.stdout.write(`Current directory: ${process.cwd()}`);
   }
-  const cmd = data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 });
+
+module.exports = pwd;
